62759fa3d7ce908fad78f96a91db7b6568c8d35a
Nícholas Oliveira <nicholas.andre@10up.com>
2020-06-02 11:06:17 -0300

    Removing missing rest api

--- index.js
+++ index.js
@@ -6,13 +6,12 @@ var JSONPersistenceProvider = require('./lib/jsonPersistenceProvider');
 var Utils = require('./lib/shared/utils');
 var ErrorInfo = require('./lib/error_info');
 var App = require('./lib/app.js');
-var RestAPI = require('./lib/restAPI');
+
 module.exports = {
   Server,
   JSONPersistenceProvider,
   ServerPersistence,
   Utils,
   ErrorInfo,
-  App,
-  RestAPI,
+  App
 };
